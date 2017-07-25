77c8850c51c1f5255dd4a3885fc93125eac04f47
Max Leitch <mjleitch@gmail.com>
2017-07-25 07:33:21 +0000

    logging messages to console

--- moisture.js
+++ moisture.js
@@ -1,8 +1,9 @@
 const R = require('ramda');
-const { fromEvent, observe, chain, skipRepeats } = require('most');
+const { debounce, filter, fromEvent, merge, map, observe, chain, skipRepeats } = require('most');
 // const nodemailer = require('nodemailer');
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
+const say = require("say");
 
 var board = new five.Board({
   io: new Raspi()
@@ -12,8 +13,12 @@ const ready$ = fromEvent('ready', board);
 const moisture$ = ready$.take(1).map(() => new five.Sensor.Digital('GPIO17'))
 const data$ = chain(s => fromEvent('data', s), moisture$)
 const changes$ = skipRepeats(data$);
-observe(console.log, changes$);
-
+const msg$ = map(R.ifElse(
+  R.equals(1),
+  R.always('Holy Beep! It\'s dry in here!'),
+  R.always('Ahhh! That\'s better')), changes$);
+// observe(say, msgs$);
+observe(console.log, msg$);
 // const smtpConfig = require('./smtpConfig.json');
 // const sendMailConfig = require('./sendMailConfig.json');
 
