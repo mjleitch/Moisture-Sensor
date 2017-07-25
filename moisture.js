c3c64cc80a7a6e7ab57d329cf09f161ad33258cb
Max Leitch <mjleitch@gmail.com>
2017-07-25 09:44:53 -0600

    Update moisture.js

--- moisture.js
+++ moisture.js
@@ -1,25 +1,32 @@
 const R = require('ramda');
 const { debounce, filter, fromEvent, merge, map, observe, chain, skipRepeats } = require('most');
-// const nodemailer = require('nodemailer');
-var five = require("johnny-five");
-var Raspi = require("raspi-io");
+const five = require("johnny-five");
+const Raspi = require("raspi-io");
 const say = require("say");
 
-var board = new five.Board({
+// Setup the board
+const board = new five.Board({
   io: new Raspi()
 });
 
+// Create a ready stream
 const ready$ = fromEvent('ready', board);
+
+// Map to the sensor
 const moisture$ = ready$.take(1).map(() => new five.Sensor.Digital('GPIO17'))
+
+// Capture the data
 const data$ = chain(s => fromEvent('data', s), moisture$)
+
+// Skip repeat values
 const changes$ = skipRepeats(data$);
+
+// Choose a message
 const msg$ = map(R.ifElse(
   R.equals(1),
   R.always('Holy Beep! It\'s dry in here!'),
   R.always('Ahhh! That\'s better')), changes$);
+
+// Speak and Log
 // observe(say, msgs$);
 observe(console.log, msg$);
-// const smtpConfig = require('./smtpConfig.json');
-// const sendMailConfig = require('./sendMailConfig.json');
-
-// const transporter = nodemailer.createTransport(smtpConfig);
